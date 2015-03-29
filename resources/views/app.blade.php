a78c9d916cce60f9506c0530a65a904e8adea61a
Christian Korndörfer <info@chko.org>
2015-03-29 01:32:08 +0100

    began styling homepage

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,7 +10,7 @@
 	<link rel="stylesheet" href="/css/vendor/font-awesome.min.css">
 
 	<!-- Fonts -->
-	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Raleway:400,200,700' rel='stylesheet' type='text/css'>
 
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -46,9 +46,13 @@
       </div>
     </div>
   @else
-    <h1>Welcome</h1>
-    <p>This is the homepage</p>
-    <a href="{{ route('login') }}" class="btn btn-md btn-primary"><i class="fa fa-fw fa-sign-in"></i> Sign in</a>
-    <a href="{{ route('register') }}" class="btn btn-md btn-primary"><i class="fa fa-fw fa-edit"></i> Register</a>
+    <div id="welcome">
+      <div class="header">
+        <h1>Your vinyls new home.</h1>
+      </div>
+
+      <a href="{{ route('login') }}" class="btn btn-md btn-primary"><i class="fa fa-fw fa-sign-in"></i> Sign in</a>
+      <a href="{{ route('register') }}" class="btn btn-md btn-primary"><i class="fa fa-fw fa-edit"></i> Register</a>
+    </div>
   @endif
 @endsection
