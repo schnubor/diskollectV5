d1590d9075a589312fa2c701cf9e30a92ae3b852
Christian <christian@styla.com>
2015-05-05 18:25:34 +0200

    stats nearly done

--- public/js/charts.js
+++ public/js/charts.js
@@ -4,62 +4,105 @@
   root = typeof exports !== "undefined" && exports !== null ? exports : this;
 
   root.getStats = function(userId) {
-    var genreChart, sizeChart, timeChart;
+    var $vinyls;
     console.log('user: ' + userId);
-    genreChart = c3.generate({
-      bindto: '#genreChart',
-      data: {
-        columns: [["setosa", 1], ["versicolor", 2], ["virginica", 3]],
-        type: 'donut'
+    return $vinyls = $.ajax({
+      url: '/api/user/' + userId + '/vinyls',
+      type: 'GET',
+      dataType: 'JSON',
+      error: function(x, status, error) {
+        console.log(status);
+        return console.log(error);
       },
-      legend: {
-        show: true
-      },
-      donut: {
-        title: '1124 Vinyls',
-        label: {
-          format: function(value) {
-            return value;
+      success: function(vinyls) {
+        var genreChart, genreData, sizeChart, sizeData, sizeData_temp, timeChart, timeData, timeData_temp;
+        console.log(vinyls);
+        genreData = [];
+        sizeData = [['x'], ['sizes']];
+        sizeData_temp = [];
+        timeData = [['x'], ['vinyls']];
+        timeData_temp = [];
+        _.each(vinyls, function(vinyl) {
+          var genre, size, time;
+          genre = vinyl.genre.split(';')[0];
+          if (genre === "") {
+            genre = "unknown";
           }
-        }
-      }
-    });
-    sizeChart = c3.generate({
-      bindto: '#sizeChart',
-      data: {
-        x: 'x',
-        columns: [['x', '7 inch', '10 inch', '12 inch'], ['sizes', 30, 200, 100]],
-        types: {
-          sizes: 'bar'
-        }
-      },
-      legend: {
-        show: false
-      },
-      axis: {
-        x: {
-          type: 'categorized'
-        }
-      }
-    });
-    return timeChart = c3.generate({
-      bindto: '#timeChart',
-      data: {
-        x: 'x',
-        columns: [['x', '2010-01-01', '2011-01-01', '2012-01-01', '2013-01-01', '2014-01-01', '2015-01-01'], ['vinyls', 30, 200, 100, 400, 150, 250]]
-      },
-      axis: {
-        x: {
-          type: 'timeseries',
-          tick: {
-            format: function(x) {
-              return x.getFullYear();
+          genreData.push(genre);
+          size = vinyl.size;
+          sizeData_temp.push(size);
+          time = vinyl.releasedate.substr(vinyl.releasedate.length - 4);
+          if (time !== "") {
+            return timeData_temp.push(time);
+          }
+        });
+        genreData = _.chain(genreData).countBy().pairs().value();
+        sizeData_temp = _.chain(sizeData_temp).countBy().pairs().value();
+        _.each(sizeData_temp, function(sizeArray) {
+          sizeData[0].push(sizeArray[0]);
+          return sizeData[1].push(sizeArray[1]);
+        });
+        timeData_temp = _.chain(timeData_temp).countBy().pairs().value();
+        _.each(timeData_temp, function(timeArray) {
+          timeData[0].push(timeArray[0]);
+          return timeData[1].push(timeArray[1]);
+        });
+        genreChart = c3.generate({
+          bindto: '#genreChart',
+          data: {
+            columns: genreData,
+            type: 'donut'
+          },
+          legend: {
+            show: true
+          },
+          donut: {
+            title: vinyls.length + ' Vinyls',
+            label: {
+              format: function(value) {
+                return value;
+              }
             }
           }
-        }
-      },
-      legend: {
-        show: false
+        });
+        sizeChart = c3.generate({
+          bindto: '#sizeChart',
+          data: {
+            x: 'x',
+            columns: sizeData,
+            types: {
+              sizes: 'bar'
+            }
+          },
+          legend: {
+            show: false
+          },
+          axis: {
+            x: {
+              type: 'categorized'
+            }
+          }
+        });
+        return timeChart = c3.generate({
+          bindto: '#timeChart',
+          data: {
+            x: 'x',
+            columns: [['x', '2010-01-01', '2011-01-01', '2012-01-01', '2013-01-01', '2014-01-01', '2015-01-01'], ['vinyls', 30, 200, 100, 400, 150, 250]]
+          },
+          axis: {
+            x: {
+              type: 'timeseries',
+              tick: {
+                format: function(x) {
+                  return x.getFullYear();
+                }
+              }
+            }
+          },
+          legend: {
+            show: false
+          }
+        });
       }
     });
   };
--- resources/assets/coffee/charts.coffee
+++ resources/assets/coffee/charts.coffee
@@ -1,54 +1,99 @@
 root = exports ? this
 
 root.getStats = (userId) ->
-    console.log 'user: '+userId
+  console.log 'user: '+userId
 
-    genreChart = c3.generate
+  $vinyls = $.ajax
+    url: '/api/user/'+userId+'/vinyls'
+    type: 'GET'
+    dataType: 'JSON'
+    error: (x,status,error) ->
+      console.log status
+      console.log error
+    success: (vinyls) -> # search results received
+      console.log vinyls
+
+      genreData = []
+
+      sizeData = [
+        ['x'],
+        ['sizes']
+      ]
+      sizeData_temp = []
+
+      timeData = [
+        ['x']
+        ['vinyls']
+      ]
+      timeData_temp = []
+
+      _.each vinyls, (vinyl) ->
+        # --- Genre --------------------------------
+        genre = vinyl.genre.split(';')[0]
+        if genre == ""
+          genre = "unknown"
+        genreData.push(genre)
+
+        # --- Size --------------------------------
+        size = vinyl.size
+        sizeData_temp.push(size)
+
+        # --- Time --------------------------------
+        time = vinyl.releasedate.substr(vinyl.releasedate.length - 4)
+        if time != ""
+          timeData_temp.push(time)
+      
+      genreData = _.chain(genreData).countBy().pairs().value()
+
+      sizeData_temp = _.chain(sizeData_temp).countBy().pairs().value()
+      _.each sizeData_temp, (sizeArray) ->
+        sizeData[0].push(sizeArray[0])
+        sizeData[1].push(sizeArray[1])
+
+      timeData_temp = _.chain(timeData_temp).countBy().pairs().value()
+      _.each timeData_temp, (timeArray) ->
+        timeData[0].push(timeArray[0])
+        timeData[1].push(timeArray[1])
+
+      genreChart = c3.generate
         bindto: '#genreChart'
         data:
-            columns: [
-                ["setosa", 1],
-                ["versicolor", 2],
-                ["virginica", 3],
-            ]
-            type: 'donut'
+          columns: genreData
+          type: 'donut'
         legend:
-            show: true
+          show: true
         donut:
-            title: '1124 Vinyls'
-            label:
-                format: (value) ->
-                    return value
+          title: vinyls.length+' Vinyls'
+          label:
+            format: (value) ->
+                return value
 
-    sizeChart = c3.generate
+      sizeChart = c3.generate
         bindto: '#sizeChart'
         data:
-            x: 'x'
-            columns: [
-                ['x', '7 inch', '10 inch', '12 inch']
-                ['sizes', 30, 200, 100]
-            ]
-            types:
-                sizes: 'bar'
+          x: 'x'
+          columns: sizeData
+          types:
+            sizes: 'bar'
         legend:
-            show: false
+          show: false
         axis:
-            x:
-                type: 'categorized'
+          x:
+            type: 'categorized'
 
-    timeChart = c3.generate
+      timeChart = c3.generate
         bindto: '#timeChart'
         data:
-            x: 'x'
-            columns: [
-                ['x', '2010-01-01', '2011-01-01', '2012-01-01', '2013-01-01', '2014-01-01', '2015-01-01']
-                ['vinyls', 30, 200, 100, 400, 150, 250]
-            ]
+          x: 'x'
+          columns: [
+            ['x', '2010-01-01', '2011-01-01', '2012-01-01', '2013-01-01', '2014-01-01', '2015-01-01']
+            ['vinyls', 30, 200, 100, 400, 150, 250]
+          ]
         axis:
-            x: 
-                type: 'timeseries'
-                tick:
-                    format: (x) -> 
-                        return x.getFullYear()
+          x: 
+            type: 'timeseries'
+            tick:
+              format: (x) -> 
+                return x.getFullYear()
         legend:
-            show: false
+          show: false
