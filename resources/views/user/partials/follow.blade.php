afd8a76c3a41c6a11b364420f719acad6d782058
schnubor <info@chko.org>
2015-05-09 11:55:39 +0200

    removed follow button from own profile

--- resources/views/user/partials/follow.blade.php
+++ resources/views/user/partials/follow.blade.php
@@ -1,14 +1,16 @@
 @if(Auth::check())
-  @if($user->isFollowedBy(Auth::user()))
-    {!! Form::open([ 'route' => ['unfollow', $user->id], 'method' => 'delete' ]) !!}
-      <button class="btn btn-md btn-default" type="submit"><i class="fa fa-fw fa-minus"></i> Unfollow</button>
-    {!! Form::close() !!}
-  @else
-    {!! Form::open([ 'route' => 'follow' ]) !!}
-      {!! Form::hidden('userIdToFollow', $user->id) !!}
-      <button class="btn btn-md btn-success" type="submit"><i class="fa fa-fw fa-plus"></i> Follow</button>
-    {!! Form::close() !!}
-  @endif
+  @unless($user == Auth::user())
+    @if($user->isFollowedBy(Auth::user()))
+      {!! Form::open([ 'route' => ['unfollow', $user->id], 'method' => 'delete' ]) !!}
+        <button class="btn btn-md btn-default" type="submit"><i class="fa fa-fw fa-minus"></i> Unfollow</button>
+      {!! Form::close() !!}
+    @else
+      {!! Form::open([ 'route' => 'follow' ]) !!}
+        {!! Form::hidden('userIdToFollow', $user->id) !!}
+        <button class="btn btn-md btn-success" type="submit"><i class="fa fa-fw fa-plus"></i> Follow</button>
+      {!! Form::close() !!}
+    @endif
+  @endunless
 @else
   {!! Form::open([ 'route' => 'follow' ]) !!}
     {!! Form::hidden('userIdToFollow', $user->id) !!}
