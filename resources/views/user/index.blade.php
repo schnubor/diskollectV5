1ff7e7299683f37a64437f6d8c96394e2c4d4567
schnubor <info@chko.org>
2015-05-20 23:52:36 +0200

    new collectors cards

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -9,11 +9,16 @@
     @foreach(array_chunk($users->all(), 4) as $userRow)
       <div class="row">
         @foreach($userRow as $user)
-          <div class="col-sm-3 text-center collector">
-            <a href="{{ route('user.show', $user->id) }}"><div class="avatar big" style="background-image: url('{{ $user->image }}');"></div></a>
-            <p class="h4"><a href="{{ route('user.show', $user->id) }}"><strong>{{ $user->username }}</strong></a></p>
-            <p>{{ $user->vinyls->count() }} Records</p>
-            @include('user.partials.follow')
+          <div class="col-sm-3 collector">
+            <div class="thumbnail">
+              <a href="{{ route('user.show', $user->id) }}"><div class="avatar big" style="background-image: url('{{ $user->image }}');"></div></a>
+              <div class="caption">
+                <p class="h4"><a href="{{ route('user.show', $user->id) }}"><strong>{{ $user->username }}</strong></a></p>
+                <p>{{ $user->vinyls->count() }} Records</p>
+                <hr>
+                <div class="text-left">@include('user.partials.follow')</div>
+              </div>
+            </div>
           </div>
         @endforeach
       </div>
