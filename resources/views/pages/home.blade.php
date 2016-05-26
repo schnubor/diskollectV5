72aa1d012d7c1b79ce6dbc1b366f0935232e4963
schnubor <info@chko.org>
2016-05-26 23:38:14 +0200

    updated styles for home page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -12,34 +12,6 @@
         <source src="/images/bg_clip.webm" type="video/webm">
         <source src="/images/bg_clip.mp4" type="video/mp4">
       </video>
-      <div class="header-menu container">
-          <nav class="navbar navbar-default navbar-static-top">
-              <div class="container-fluid">
-                <!-- Brand and toggle get grouped for better mobile display -->
-                <div class="navbar-header">
-                  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                  </button>
-                </div>
-
-                <!-- Collect the nav links, forms, and other content for toggling -->
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                  <ul class="nav navbar-nav">
-                    <li><a href="#features">Features</a></li>
-                    <li><a href="#">About</a></li>
-                  </ul>
-
-                  <ul class="nav navbar-nav navbar-right">
-                    <li><a href="{{ route('login') }}">Login</a></li>
-                    <li><a href="{{ route('register') }}" class="btn btn-default btn-sm"><i class="fa fa-edit"></i> Register</a></li>
-                  </ul>
-                </div><!-- /.navbar-collapse -->
-              </div><!-- /.container-fluid -->
-            </nav>
-      </div>
       <div class="header-content container">
         <div class="logo">
           <img src="/images/logo.png" alt="For the record">
